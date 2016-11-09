3b6b85329553e611287ef653c63345ec6207e789
RoseReid <rosemaryalicereid@gmail.com>
2016-11-09 10:10:40 +0100

    schema refactoring

--- models/evaluation.js
+++ models/evaluation.js
@@ -10,27 +10,32 @@ db.once('open', function() {
 	console.log('connected evaluation')
 });
 
-const ninjaInfoSchema = new mongoose.Schema({
-	ninjaName: {type:String, required:true},
+const ninjaSchema = new mongoose.Schema({
+	name: {type:String, required:true},
+	email: {type: String, set: toLower, required: true}
 });
 
-const clientInfoSchema = new mongoose.Schema({
-	clientName: {type:String, required:true},
+const clientSchema = new mongoose.Schema({
+	name: {type:String, required:true},
 });
 
 //change to grade
-const evaluation = {
-	happyFace: {type:Boolean, required:true},
-	mehFace: {type:Boolean, required:true},
-	sadFace: {type:Boolean, required:true},
-	question: {type:String, required:true}
+const scale = {
+	//use number to identify the happyFace .....
+	0: {type:Boolean, required:true},
+	1: {type:Boolean, required:true},
+	2: {type:Boolean, required:true},
+	template: {type:String, required:true}
 };
 
 const evaluationSchema = new mongoose.Schema({
-	evaluation: {type: evaluation, required: true},
-	ninja: {type: ninjaInfoSchema, required: true},
-	client: {type: clientInfoSchema, required: true} 
-}, {timestamps: {updatedAt: "dateSaved"}});
+	//answers a better word and answers should contain a collection with tupples containing question and answer.
+
+	answers: {type: scale, required: true},
+	ninja: {type: ninjachema, required: true},
+	client: {type: clientSchema, required: true} 
+}, {timestamps: {updatedAt: "dateSaved"}
+);
 
 exports.Model = db.model('evaluation', evaluationSchema);
 
